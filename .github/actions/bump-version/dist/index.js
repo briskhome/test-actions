75f39e5fe7ff80c033b3e3b1db03019d879e66cd
Egor Zaitsev <heuels@gmail.com>
2020-07-02 22:01:47 +0300

    wip

--- .github/actions/bump-version/dist/index.js
+++ .github/actions/bump-version/dist/index.js
@@ -706,12 +706,12 @@ async function run() {
     const { owner, repo } = context.repo;
     core.debug(`${actor}:${token}@github.com/${owner}/${repo}.git`)
     // Configure git
-    await exec("git", [
-      "remote",
-      "add",
-      "github",
-      `${actor}:${token}@github.com/${owner}/${repo}.git`,
-    ]);
+    // await exec("git", [
+    //   "remote",
+    //   "add",
+    //   "github",
+    //   `${actor}:${token}@github.com/${owner}/${repo}.git`,
+    // ]);
     await exec("git", ["config", "user.name", '"Osome Bot"']);
     await exec("git", ["config", "user.email", '"heuels@osome.com"']);
 
@@ -726,7 +726,7 @@ async function run() {
     // Create a commit and push it
     await exec("git", ["add", "package.json"]);
     await exec("git", ["commit", "-m", `"v1.2.3"`]);
-    await exec("git", ["push", "github", `HEAD:${context.ref}`]);
+    await exec("git", ["push", "origin", `HEAD:${context.ref}`]);
   } catch (error) {
     core.setFailed(error.message);
   }
