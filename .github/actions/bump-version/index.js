2ae603edc61a3d72727958cea1d2d77977a0fa36
Egor Zaitsev <heuels@gmail.com>
2020-07-02 21:50:14 +0300

    wip

--- .github/actions/bump-version/index.js
+++ .github/actions/bump-version/index.js
@@ -1,39 +1,44 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { exec } = require('@actions/exec');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { exec } = require("@actions/exec");
 
 async function run() {
   try {
-    const actor = core.getInput('actor');
-    const token = core.getInput('token');
+    const actor = core.getInput("actor");
+    const token = core.getInput("token");
     const context = github.context;
-
+    core.debug(`"${actor}:${token}@github.com/${context.repository}.git"`)
     // Configure git
-    await exec('git', ['remote', 'add', 'github', `"${actor}:${token}@github.com/${context.repository}.git"`]);
-    await exec('git', ['config', 'user.name', '"Osome Bot"']);
-    await exec('git', ['config', 'user.email', '"heuels@osome.com"']);
+    await exec("git", [
+      "remote",
+      "add",
+      "github",
+      `"${actor}:${token}@github.com/${context.repository}.git"`,
+    ]);
+    await exec("git", ["config", "user.name", '"Osome Bot"']);
+    await exec("git", ["config", "user.email", '"heuels@osome.com"']);
 
     // Bump minor version
-    await exec('npm', ['version', 'minor', '--no-git-tag-version']);
-    let myOutput = '';
-    const options = {};
-    options.listeners = {
-      stdout: (data) => {
-        myOutput += data.toString();
+    let version = "";
+    await exec("npm", ["version", "minor", "--no-git-tag-version"], {
+      listeners: {
+        stdout: (data) => {
+          myOutput += data.toString();
+        },
       },
-    };
+    });
 
-    await exec('ls -la', [], options)
-    core.debug(`myOutput: ${myOutput}`);
+    await exec("ls -la", [], options);
+    core.debug(version);
     // const version = require('./package.json').version;
 
     // Create a commit and push it
-    await exec('git', ['add', 'package.json']);
-    await exec('git', ['commit', '-m', `"v1.2.3"`]);
-    await exec('git', ['push', 'github', `HEAD:${context.ref}`]);
+    await exec("git", ["add", "package.json"]);
+    await exec("git", ["commit", "-m", `"v1.2.3"`]);
+    await exec("git", ["push", "github", `HEAD:${context.ref}`]);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
