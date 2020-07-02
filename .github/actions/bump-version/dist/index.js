8e3912e595245820160e207dfff0e9c57436e3e5
Egor Zaitsev <heuels@gmail.com>
2020-07-02 22:07:27 +0300

    wip

--- .github/actions/bump-version/dist/index.js
+++ .github/actions/bump-version/dist/index.js
@@ -704,15 +704,8 @@ async function run() {
     const token = core.getInput("token");
     const context = github.context;
     const { owner, repo } = context.repo;
-    core.debug(`${actor}:${token}@github.com/${owner}/${repo}.git`)
-    // Configure git
-    // await exec("git", [
-    //   "remote",
-    //   "add",
-    //   "github",
-    //   `${actor}:${token}@github.com/${owner}/${repo}.git`,
-    // ]);
 
+    // Configure git
     await exec("git", ['remote', 'set-url', 'origin', `https://${actor}:${token}@github.com/${owner}/${repo}.git`]);
     await exec("git", ["config", "user.name", '"Osome Bot"']);
     await exec("git", ["config", "user.email", '"heuels@osome.com"']);
@@ -723,11 +716,9 @@ async function run() {
       listeners: { stdout: (data) => version += data.toString() },
     });
 
-    core.debug(version);
-
     // Create a commit and push it
     await exec("git", ["add", "package.json"]);
-    await exec("git", ["commit", "-m", `"v1.2.3"`]);
+    await exec("git", ["commit", "-m", `v${version}`]);
     await exec("git", ["push", "origin", `HEAD:${context.ref}`]);
   } catch (error) {
     core.setFailed(error.message);
